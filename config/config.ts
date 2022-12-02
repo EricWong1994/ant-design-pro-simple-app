7425fb870ba6dd1c75f3b1b0e0e83c246b2e63b8
搬砖达人小王git-flow <461864051@qq.com>
2022-12-02 22:50:47 +0800

    feat: 初始化antd配置 解决openapi报错

--- config/config.ts
+++ config/config.ts
@@ -14,36 +14,14 @@ export default defineConfig({
   dva: {
     hmr: true,
   },
-  // layout: {
-  //   // https://umijs.org/zh-CN/plugins/plugin-layout
-  //   locale: true,
-  //   siderWidth: 208,
-  //   ...defaultSettings,
-  // },
   layout: {
-    // https://umijs.org/zh-CN/plugins/plugin-layout
     locale: true,
     siderWidth: 208,
     ...defaultSettings,
   },
-  // layout:{
-  //   navTheme: "light",
-  //   primaryColor: "#52C41A",
-  //   layout: "mix",
-  //   contentWidth: "Fluid",
-  //   fixedHeader: false,
-  //   fixSiderbar: true,
-  //   pwa: false,
-  //   logo: "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
-  //   headerHeight: 48,
-  //   splitMenus: false
-  // },
-  // https://umijs.org/zh-CN/plugins/plugin-locale
   locale: {
-    // default zh-CN
     default: 'zh-CN',
     antd: true,
-    // default true, when it is true, will use `navigator.language` overwrite default
     baseNavigator: true,
   },
   dynamicImport: {
@@ -52,14 +30,10 @@ export default defineConfig({
   targets: {
     ie: 11,
   },
-  // umi routes: https://umijs.org/docs/routing
   routes,
-  // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: {
     'root-entry-name': 'variable',
   },
-  // esbuild is father build tools
-  // https://umijs.org/plugins/plugin-esbuild
   esbuild: {},
   title: false,
   ignoreMomentLocale: true,
@@ -67,13 +41,11 @@ export default defineConfig({
   manifest: {
     basePath: '/',
   },
-  // Fast Refresh 热更新
   fastRefresh: {},
   openAPI: [
     {
       requestLibPath: "import { request } from 'umi'",
-      // 或者使用在线的版本
-      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
+      // 或者使用在线的版本 schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
       schemaPath: join(__dirname, 'oneapi.json'),
       mock: false,
     },
--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -4,30 +4,18 @@ const Settings: LayoutSettings & {
   pwa?: boolean;
   logo?: string;
 } = {
-  // navTheme: 'light',
-  // // 拂晓蓝
-  // primaryColor: '#1890ff',
-  // layout: 'mix',
-  // contentWidth: 'Fluid',
-  // fixedHeader: false,
-  // fixSiderbar: true,
-  // colorWeak: false,
-  // title: 'Ant Design Pro',
-  // pwa: false,
-  // logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-  // iconfontUrl: '',
-
-    navTheme: "light",
-    primaryColor: "#52C41A",
-    // layout: "mix",
-    layout: "side",
-    contentWidth: "Fluid",
-    fixedHeader: false,
-    fixSiderbar: true,
-    pwa: false,
-    logo: "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
-    headerHeight: 48,
-    splitMenus: false
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'Ant Design Pro',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
 };
 
 export default Settings;
